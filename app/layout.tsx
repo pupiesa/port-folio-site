8bd116ad926ecb118196e71e157d1f7a418abf5c
Dutsakorn Tubsang <66200082@kmitl.ac.th>
2025-02-06 07:38:27 +0700

    add: vercel analytic on root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Nav />
         <main className="mt-[4rem] md:mt=[7rem]">
           {children}
+          <Analytics/>
         </main>
         <Footer  />
       </body>
