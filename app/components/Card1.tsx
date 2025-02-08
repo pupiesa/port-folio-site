0255546ba9b91f6d1387f7ee219d37692690364e
Dutsakorn Tubsang <66200082@kmitl.ac.th>
2025-02-08 11:09:01 +0700

    add:modify card to displying info by using props

--- app/components/Card1.tsx
+++ app/components/Card1.tsx
@@ -3,29 +3,33 @@ import React from "react";
 import { gothic } from "../fonts";
 import Link from "next/link";
 
-const Card1 = () => {
+interface Card1Props {
+  names: string;
+  desc: string;
+  imgs: string;
+  links: string;
+  gittlinks: string;
+}
+
+const Card1: React.FC<Card1Props> = ({ names,desc,imgs,links,gittlinks }) => {
   return (
     <div className="flex flex-col rounded-[20px] border-solid border-2 border-black sm:max-w-[300px] lg:max-w-none sm:mx-auto">
       <div>
         <Image
-          src="/pokedex.svg"
+          src={imgs}
           alt="pokedex image"
           width={180}
           height={180}
           className="w-[100%] h-[100%] rounded-t-[20px] bg-[#4A628A]"
         ></Image>
       </div>
-      <div className={`${gothic.className} bg-[#4A628A] flex flex-col p-4 rounded-b-[20px] gap-y-3 `}>
-        <div className="text-[2em]">Pokedex</div>
-        <div className="text-[1em] tracking-wide">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit
+      <div className={`${gothic.className} bg-[#4A628A] flex flex-col p-4 rounded-b-[20px] gap-y-3  h-[100%]`}>
+        <div className="text-[2em]">{names}</div>
+        <div className="text-[1em] tracking-wide h-[6em]">
+          {desc}
         </div>
         <div className="flex flew-row justify-end gap-x-4">
-          <Link href={"https://pokedex.software"} target="blank">
+          <Link href={links} target="blank">
             <Image
               src="/internet.svg"
               alt="internet Icon"
@@ -34,7 +38,7 @@ const Card1 = () => {
               className="w-[auto] h-[1.5em]"
             ></Image>
           </Link>
-          <Link href={"https://github.com/pupiesa/Pokedex"} target="blank">
+          <Link href={gittlinks} target="blank">
             <Image
               src="/git.svg"
               alt="github Icon"
--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -3,6 +3,8 @@ import { lateef } from "../fonts";
 import Card1 from "./Card1";
 
 const Work = () => {
+  const pokedex = ['Pokedex','A Next.js project practicing API integration, fetching and displaying Pokémon data from PokeAPI. Styled with Tailwind CSS and deployed on Vercel','/pokedex.svg','https://pokedex.software',"https://github.com/pupiesa/Pokedex"]
+  const premid = ['Dek-d Premid Presence',`Contributed to this open-source browser extension that displays the current browsing website on a user's Discord status. Added support for Dek-D by implementing TypeScript for site detection and integration.`,'/dekd.svg','https://premid.app/store/presences/Dek-d','https://github.com/PreMiD/Presences']
   return (
     <>
       {/* work */}
@@ -10,10 +12,10 @@ const Work = () => {
         <div className="font-bold text-[4rem]">My Work</div>
         <div className="">Check out what I’ve been working on</div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-[2rem] gap-y-8 lg:gap-x-[3rem]  h-[auto]">
-        <Card1 />
-        <Card1 />
-        <Card1 />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-[2rem] gap-y-8 lg:gap-x-[2rem] sm:px[6rem] h-[auto]">
+        <Card1 names={pokedex[0]} desc={pokedex[1]} imgs={pokedex[2]} links={pokedex[3]} gittlinks={pokedex[4]}/>
+        <Card1 names={premid[0]} desc={premid[1]} imgs={premid[2]} links={premid[3]} gittlinks={premid[4]}/>
+        <Card1 names={pokedex[0]} desc={pokedex[1]} imgs={pokedex[2]} links={pokedex[3]} gittlinks={pokedex[4]}/>
       </div>
     </>
   );
